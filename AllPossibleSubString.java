9de9dead419b601b5d570321a6ed91163cef8679
Harshita Singh <30619470+HarshitaSing@users.noreply.github.com>
2018-08-18 21:20:03 +0530

    Add files via upload
    Correct Program

--- AllPossibleSubString.java
+++ AllPossibleSubString.java
@@ -1,6 +1,8 @@
 package StringPrograms;
 
 import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class AllPossibleSubString {
 
@@ -11,19 +13,16 @@ public class AllPossibleSubString {
 		S = sc.next();
 		int k = 3;
 		int i;
-		String[] arr= {} ;
-		//SortedSet<String> s = new TreeSet<String>();
+		//String[] arr= {} ;
+		SortedSet<String> s = new TreeSet<String>();
 		for(i =0;i<=S.length()-k;i++)
 		{
-			 arr[i]=(S.substring(i, i+k));
+			s.add(S.substring(i, i+k));
 		}
-		//System.out.println(s.first());
-		//System.out.println(s.last());
+		System.out.println(s.first());
+		System.out.println(s.last());
 		//System.out.println(arr);
-		for(i =0;i<S.length()-k;i++)
-		{
-			System.out.println(arr[i]);
-		}
+		
 		
 		
 
